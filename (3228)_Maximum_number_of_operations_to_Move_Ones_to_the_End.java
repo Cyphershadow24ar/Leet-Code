8c0ffbf46011c1d904275e273da272423be6b5c7
Aniket Raj <aniketraj0224@gmail.com>
2025-11-13 11:09:20 +0530

    Create (3228)_Maximum_number_of_operations_to_Move_Ones_to_the_End.java

---
+++ (3228)_Maximum_number_of_operations_to_Move_Ones_to_the_End.java
@@ -0,0 +1,24 @@
+// PROBLEM : (3228) Maximum Number of Operations to Move Ones to the End
+
+// SOLUTION :
+
+public class Solution {
+
+    public int maxOperations(String s) {
+        int countOne = 0;
+        int ans = 0;
+        int i = 0;
+        while (i < s.length()) {
+            if (s.charAt(i) == '0') {
+                while (i + 1 < s.length() && s.charAt(i + 1) == '0') {
+                    i++;
+                }
+                ans += countOne;
+            } else {
+                countOne++;
+            }
+            i++;
+        }
+        return ans;
+    }
+}
