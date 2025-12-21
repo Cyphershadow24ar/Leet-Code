c3900f2bba1393e56f02d23c5b84b374b803b60f
Aniket Raj <aniketraj0224@gmail.com>
2025-12-22 00:35:59 +0530

    Create 1572_Matrix_diagonal_Sum.java

---
+++ 1572_Matrix_diagonal_Sum.java
@@ -0,0 +1,18 @@
+// PROBLEM : (1572) Matrix Diagonal Sum
+
+// SOLUTION : 
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+       
+       for(int i = 1 ; i < nums.length; i++){
+        for(int j = 0 ; j <  nums.length - i; j++){
+            if(nums[j] + nums[j+i] == target)
+            {
+                return new int[]{j,j + i};
+            }
+        }
+       }
+    return new int[]{};
+    }
+}
