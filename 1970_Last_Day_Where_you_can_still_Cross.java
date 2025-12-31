4f83ea4a37c875d662a6cc0b04c0113e19a19664
Aniket Raj <aniketraj0224@gmail.com>
2025-12-31 10:24:56 +0530

    Update 1970_Last_Day_Where_you_can_still_Cross.java

--- 1970_Last_Day_Where_you_can_still_Cross.java
+++ 1970_Last_Day_Where_you_can_still_Cross.java
@@ -2,65 +2,92 @@
 
 // SOLUTION :
 
-import java.util.*;
 
 class Solution {
-    private int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
-    public int latestDayToCross(int row, int col, int[][] cells) {
-        int left = 1, right = cells.length;
-        int ans = 0;
+    private int[] rank;
+    private int[] root;
+    private int row;
+    private int col;
+    private static final int[][] DIRECTIONS = new int[][] {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1}};
+    private int leftWaterId;
+    private int rightWaterId;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (canCross(row, col, cells, mid)) {
-                ans = mid; // Try to find a later day
-                left = mid + 1;
-            } else {
-                right = mid - 1; // Must be an earlier day
+    public int latestDayToCross(int row, int col, int[][] cells) { //DSU on water cells solution 
+        this.row = row;
+        this.col = col;
+        rank = new int[(row * col) + 2]; // +1 is left side root which every water cell on left side will be connected to, +2 is right side 
+        root = new int[(row * col) + 2]; // +1 is left side root which every water cell on left side will be connected to, +2 is right side
+        leftWaterId = (row * col); // read above
+        rightWaterId = (row * col) + 1; // read above
+        root[leftWaterId] = leftWaterId; // read above
+        root[rightWaterId] = rightWaterId; // read above
+        
+        for(int i=0; i<row; i++) {
+            for(int j=0; j<col; j++) {
+                root[toId(i, j)] = toId(i, j);
             }
         }
-        return ans;
-    }
+        boolean[][] water = new boolean[row][col];
 
-    private boolean canCross(int row, int col, int[][] cells, int day) {
-        int[][] grid = new int[row][col];
-        // Mark water cells up to the given day
-        for (int i = 0; i < day; i++) {
-            grid[cells[i][0] - 1][cells[i][1] - 1] = 1;
-        }
+        for(int i=0; i<cells.length; i++) {
+            int x = cells[i][0]-1;
+            int y = cells[i][1]-1;
 
-        Queue<int[]> queue = new LinkedList<>();
-        boolean[][] visited = new boolean[row][col];
+            water[x][y] = true;
 
-        // Start BFS from all land cells in the first row
-        for (int c = 0; c < col; c++) {
-            if (grid[0][c] == 0) {
-                queue.offer(new int[]{0, c});
-                visited[0][c] = true;
+            if(y == 0) {
+                union(toId(x, y), leftWaterId);
             }
-        }
-
-        while (!queue.isEmpty()) {
-            int[] curr = queue.poll();
-            int r = curr[0];
-            int c = curr[1];
-
-            // If we reached the last row, crossing is possible
-            if (r == row - 1) return true;
-
-            for (int[] dir : directions) {
-                int nr = r + dir[0];
-                int nc = c + dir[1];
-
-                if (nr >= 0 && nr < row && nc >= 0 && nc < col 
-                    && grid[nr][nc] == 0 && !visited[nr][nc]) {
-                    visited[nr][nc] = true;
-                    queue.offer(new int[]{nr, nc});
+            else if(y == col-1) {
+                union(toId(x, y), rightWaterId);
+            }
+            
+            for(int[] dir: DIRECTIONS) {
+                int newx = x + dir[0];
+                int newy = y + dir[1];
+                if(valid(newx, newy) && water[newx][newy]) {
+                    union(toId(x, y), toId(newx, newy));
                 }
             }
+            if(find(leftWaterId) == find(rightWaterId)) {
+                return i;
+            }
         }
 
-        return false;
+        return -1;
     }
+
+    private int toId(int x, int y) {
+        return x * col + y;
+    }
+
+    private void union(int id1, int id2) {
+        int root1 = find(id1);
+        int root2 = find(id2);
+
+        if(rank[root1] == rank[root2]) {
+            rank[root1]++;
+            root[root2] = root[root1];
+        }
+        else if(rank[root1] > rank[root2]) {
+            root[root2] = root[root1];
+        }
+        else {
+            root[root1] = root[root2];
+        }
+    }
+
+    private int find(int id) {
+        while(root[id] != id) {
+            root[id] = root[root[id]];
+            id = root[id];
+        }
+        return id;
+    }
+
+    private boolean valid(int x, int y) {
+        return x >= 0 && x < row && y >= 0 && y < col;
+    }
+
 }
