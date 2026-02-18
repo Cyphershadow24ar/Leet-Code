5a96c35138f475c57d0509836a84f54d390bbb6f
Aniket Raj <aniketraj0224@gmail.com>
2026-02-18 13:10:33 +0530

    Create 693_Binary_Number_with_Alternating_Bits.java

---
+++ 693_Binary_Number_with_Alternating_Bits.java
@@ -0,0 +1,12 @@
+// PROBLEM : (693) Binary Number with Alternating Bits
+
+// SOLUTION :
+
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+
+        int x = n ^ (n >> 1);
+
+        return (x & (x + 1)) == 0;
+    }
+}
