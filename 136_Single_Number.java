07a9ef870a303ec0c01504e22c68c58f3c979d80
Aniket Raj <aniketraj0224@gmail.com>
2026-03-07 11:48:19 +0530

    Update 136_Single_Number.java

--- 136_Single_Number.java
+++ 136_Single_Number.java
@@ -2,6 +2,8 @@
 
 // SOLUTION : 
 
+// 1. Using Bit Manipulation 
+
 class Solution {
     public int singleNumber(int[] nums) {
         int result =0;
@@ -9,5 +11,22 @@ class Solution {
             result ^= num;
         }
         return result;
+    }
+}
+
+
+// 2. Linear Search 
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        
+        for(int i =0; i<n-1; i+=2){
+            if(nums[i] != nums[i+1]){ 
+                return nums[i];
+            }
+        }
+        return nums[nums.length - 1];
     }
 }
