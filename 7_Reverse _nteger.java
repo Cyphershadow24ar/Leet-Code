10125c7366d78b4db56734d40b623c8166facd98
Aniket Raj <aniketraj0224@gmail.com>
2026-03-08 21:16:05 +0530

    Create 7_Reverse _nteger.java

---
+++ 7_Reverse _nteger.java
@@ -0,0 +1,20 @@
+// PROBLEM : (7) Reverse Integer
+
+// SOLUTION : 
+
+class Solution {
+    public int reverse(int x) {
+        long rev = 0;
+        
+        while(x != 0){
+            int r = x % 10; 
+            rev = (rev * 10) + r;
+            x /= 10;
+        }
+         if ( rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE){
+                return 0;
+        }
+        return (int) rev;
+    }
+
+}
