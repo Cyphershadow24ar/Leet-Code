02be6cbbe86be72f69a3e3ff6635390adefb5ca1
Aniket Raj <aniketraj0224@gmail.com>
2026-05-04 13:37:54 +0530

    Create 48 Rotate Image.java

---
+++ 48 Rotate Image.java
@@ -0,0 +1,27 @@
+// PROBLEM : (48) Rotate Image
+
+// SOLUTION :
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        
+        // Step 1: Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        
+        // Step 2: Reverse each row
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+    }
+}
