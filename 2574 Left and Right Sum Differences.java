f25dd610b88216241a8a92dfcdd651f59eaa6744
Aniket Raj <aniketraj0224@gmail.com>
2026-06-06 20:33:53 +0530

    Create 2574 Left and Right Sum Differences.java

---
+++ 2574 Left and Right Sum Differences.java
@@ -0,0 +1,20 @@
+// PROBLEM : (2574) Left and Right Sum Differences
+
+// SOLUTION :
+
+class Solution:
+    def leftRightDifference(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        ans = [0] * n
+
+        left_sum = 0
+        for i in range(n):
+            ans[i] = left_sum
+            left_sum += nums[i]
+
+        right_sum = 0
+        for i in range(n - 1, -1, -1):
+            ans[i] = abs(ans[i] - right_sum)
+            right_sum += nums[i]
+
+        return ans
