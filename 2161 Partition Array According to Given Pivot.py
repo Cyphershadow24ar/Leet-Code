b1dfbf5b729e53ecb0002cb0084a54c2d13c314a
Aniket Raj <aniketraj0224@gmail.com>
2026-06-08 23:15:29 +0530

    Create 2161 Partition Array According to Given Pivot.py

---
+++ 2161 Partition Array According to Given Pivot.py
@@ -0,0 +1,24 @@
+# PROBLEM : (2161) Partition Array According to Given Pivot
+
+# SOLUTION :
+
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        less = []
+        equal = [] 
+        greater = []
+
+        for num in nums:
+            if num < pivot:
+                less.append(num)
+            elif num > pivot:
+                greater.append(num)
+            else:
+                equal.append(num)
+
+        less.extend(equal)
+        less.extend(greater)
+
+        return less
+
+
