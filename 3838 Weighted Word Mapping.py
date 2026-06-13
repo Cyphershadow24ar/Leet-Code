b3244b77722c72ffafda1479daf21b9ec49eec20
Aniket Raj <aniketraj0224@gmail.com>
2026-06-14 02:48:24 +0530

    Create 3838 Weighted Word Mapping.py

---
+++ 3838 Weighted Word Mapping.py
@@ -0,0 +1,13 @@
+# PROBLEM : (3838) Weighted Word Mapping
+
+# SOLUTION :
+
+class Solution:
+    def mapWordWeights(self, words: List[str], weights: List[int]) -> str:
+        ans = []
+        for word in words:
+            s = 0
+            for c in word:
+                s += weights[ord(c) - ord("a")]
+            ans.append(chr(ord("z") - s % 26))
+        return "".join(ans)
