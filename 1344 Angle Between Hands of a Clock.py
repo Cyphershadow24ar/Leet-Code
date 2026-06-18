c421ea5d2fe6d2e92d3446a73a1f9984f4592ddf
Aniket Raj <aniketraj0224@gmail.com>
2026-06-18 15:08:23 +0530

    Add solution for angle between clock hands problem

---
+++ 1344 Angle Between Hands of a Clock.py
@@ -0,0 +1,9 @@
+# PROBLEM : (1344) Angle Between Hands of a Clock
+
+# SOLUTION :
+
+class Solution:
+    def angleClock(self, hour: int, minutes: int) -> float:
+        x = hour + minutes / 60
+        diff = (11 * x) % 12
+        return min(diff, 12 - diff) * 30
