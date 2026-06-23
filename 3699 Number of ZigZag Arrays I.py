b419be71df868e534d262aad1fbc47a4469a3495
Aniket Raj <aniketraj0224@gmail.com>
2026-06-23 22:19:21 +0530

    Create 3699 Number of ZigZag Arrays I.py

---
+++ 3699 Number of ZigZag Arrays I.py
@@ -0,0 +1,17 @@
+# PROBLEM : (3699) Number of ZigZag Arrays I
+
+# SOLUTION :
+
+class Solution:
+    def zigZagArrays(self, n: int, l: int, r: int) -> int:
+        MOD = 1000000007
+        m = r - l + 1
+        dp = [1] * m
+
+        for i in range(2, n + 1):
+            dp.reverse()
+            s = 0
+            for j in range(m):
+                dp[j], s = s, (s + dp[j]) % MOD
+
+        return (sum(dp) % MOD << 1) % MOD
