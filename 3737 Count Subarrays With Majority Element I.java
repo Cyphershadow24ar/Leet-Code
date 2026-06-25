ca18b46468ac4596c64c43065651abda23bd90c3
Aniket Raj <aniketraj0224@gmail.com>
2026-06-25 12:55:54 +0530

    Create 3737 Count Subarrays With Majority Element I.java

---
+++ 3737 Count Subarrays With Majority Element I.java
@@ -0,0 +1,21 @@
+// PROBLEM : (3737) Count Subarrays With Majority Element I
+
+// SOLUTION :
+
+class Solution {
+
+    public int countMajoritySubarrays(int[] nums, int target) {
+        int n = nums.length;
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            int cnt = 0;
+            for (int j = i; j < n; ++j) {
+                cnt += (nums[j] == target ? 1 : -1);
+                if (cnt > 0) {
+                    ++ans;
+                }
+            }
+        }
+        return ans;
+    }
+}
