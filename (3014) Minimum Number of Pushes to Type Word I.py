9a2764c973ba227b2fadce899715facf6268d56b
Aniket Raj <aniketraj0224@gmail.com>
2026-07-30 15:04:55 +0530

    Delete (3014) Minimum Number of Pushes to Type Word I.py

--- (3014) Minimum Number of Pushes to Type Word I.py
+++
@@ -1,8 +0,0 @@
-# PROBLEM : (3014) Minimum Number of Pushes to Type Word I
-
-# SOLUTION : 
-
-class Solution:
-    def minimumPushes(self, A: str) -> int:
-        q, r = divmod(len(A), 8)
-        return ((q << 2) + r) * (q + 1)
