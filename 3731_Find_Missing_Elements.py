fac1b861fdb7ca70e1d8b5efd00f026a9cdd3a18
Aniket Raj <aniketraj0224@gmail.com>
2026-08-04 15:51:13 +0530

    Create 3731_Find_Missing_Elements.py

---
+++ 3731_Find_Missing_Elements.py
@@ -0,0 +1,15 @@
+# PROBLEM : (3731) Find Missing Elements
+
+# SOLUTION :
+
+class Solution:
+    def findMissingElements(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        a=[]
+        j=0
+        for i in range(nums[0],nums[-1]):
+            if nums[j]!=i:
+                a.append(i)
+            else:
+                j+=1
+        return a
